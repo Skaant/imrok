4d43256220cff98e6ce963290c809d2a032266b4
Rimarok <romaric.ruga@gmail.com>
2022-09-26 14:13:31 +0200

    c/RichTextRender : add href support

--- src/components/RichTextRenderer.tsx
+++ src/components/RichTextRenderer.tsx
@@ -8,11 +8,13 @@ function RichTextRenderer({
 }): React.ReactElement {
   return (
     <>
-      {richTexts.map(({ annotations, plain_text }) => {
+      {richTexts.map(({ annotations, plain_text, href }) => {
         if (annotations.bold) {
           return <b>{plain_text}</b>;
         } else if (annotations.italic) {
           return <i>{plain_text}</i>;
+        } else if (href) {
+          return <a href={href}>{plain_text}</a>;
         }
         return plain_text;
       })}
