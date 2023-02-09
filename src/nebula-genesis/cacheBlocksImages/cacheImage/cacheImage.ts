405b79b310b70fe9f5fb8c08b0f13f4871233e8e
Tim <Tim280696@hotmail.com>
2023-02-09 11:58:26 +0100

    cacheImage: add console.log

--- src/nebula-genesis/cacheBlocksImages/cacheImage/cacheImage.ts
+++ src/nebula-genesis/cacheBlocksImages/cacheImage/cacheImage.ts
@@ -1,6 +1,6 @@
 import { stat, writeFile } from "fs/promises";
-import * as sharp from "sharp";
 import fetch from "node-fetch";
+const sharp = require("sharp");
 
 export default async function cacheImage(imageUrl: string, siteUrl: string) {
   const filename = imageUrl.split("?")[0].split("/").pop() || "";
@@ -11,13 +11,15 @@ export default async function cacheImage(imageUrl: string, siteUrl: string) {
     } catch (err) {
       const res = await fetch(`${siteUrl}/static/medias/${filename}`);
       if (res.status === 404) {
+        console.log(filename);
         const res = await fetch(imageUrl);
+        console.log("res");
         const buffer = await res.buffer();
+        console.log("buffer");
         await writeFile(filepath, buffer);
+        console.log("writefile");
 
-        const metadata = await sharp(
-          `src/static/medias/${filename}`
-        ).metadata();
+        const metadata = await sharp(filepath).metadata();
 
         const sizes = [
           {
@@ -31,7 +33,7 @@ export default async function cacheImage(imageUrl: string, siteUrl: string) {
         ];
 
         if (metadata.width && metadata.width >= 360 && metadata.width <= 800) {
-          await sharp(`src/static/medias/${filename}`)
+          await sharp(filepath)
             .rotate()
             .resize(sizes[1].width)
             .toFile(
