fa10c5ebc207b6c0c40893d1ccb26c1e0a83dc72
Tim <Tim280696@hotmail.com>
2023-02-10 10:08:26 +0100

    cacheBlockImage: remove filter

--- src/nebula-genesis/cacheBlocksImages/cacheBlocksImages.ts
+++ src/nebula-genesis/cacheBlocksImages/cacheBlocksImages.ts
@@ -6,33 +6,31 @@ export default async function cacheBlocksImages(
   siteUrl: string
 ) {
   return await Promise.all(
-    blocks
-      .filter((block) => block.type === "image" && block.image.type === "file")
-      .map(async (block) => {
-        /* Redudant block type checking for TS typing */
-        if (block.type === "image" && block.image.type === "file") {
-          /**
-           * @todo `cacheImage` should send back multiple urls
-           *  to provide `ResizedImageBlockObject` below.
-           */
-          const filename = await cacheImage(block.image.file.url, siteUrl);
-          if (filename) {
-            block = {
-              id: block.id,
-              type: "resized_image",
-              standardUrl: `/static/medias/${filename}`,
-              minUrl: `src/static/medias/${filename.replace(
-                /(\.[\w\d_-]+)$/i,
-                "--min$1"
-              )}`,
-              medUrl: `src/static/medias/${filename.replace(
-                /(\.[\w\d_-]+)$/i,
-                "--med$1"
-              )}`,
-            } as ResizedImageBlockObject;
-          }
+    blocks.map(async (block) => {
+      /* Redudant block type checking for TS typing */
+      if (block.type === "image" && block.image.type === "file") {
+        /**
+         * @todo `cacheImage` should send back multiple urls
+         *  to provide `ResizedImageBlockObject` below.
+         */
+        const filename = await cacheImage(block.image.file.url, siteUrl);
+        if (filename) {
+          block = {
+            id: block.id,
+            type: "resized_image",
+            standardUrl: `/static/medias/${filename}`,
+            minUrl: `src/static/medias/${filename.replace(
+              /(\.[\w\d_-]+)$/i,
+              "--min$1"
+            )}`,
+            medUrl: `src/static/medias/${filename.replace(
+              /(\.[\w\d_-]+)$/i,
+              "--med$1"
+            )}`,
+          } as ResizedImageBlockObject;
         }
-        return block;
-      })
+      }
+      return block;
+    })
   );
 }
